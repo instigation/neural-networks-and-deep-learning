19a4541fa2a6ea4dd7fda10d4884176c59dbbe73
instigation <instigation@naver.com>
2017-05-21 22:51:16 +0900

    floyd 위에서 돌아갈 basic instructions 만듬

--- src/mnist_loader.py
+++ src/mnist_loader.py
@@ -16,7 +16,8 @@ import gzip
 # Third-party libraries
 import numpy as np
 
-def load_data():
+
+def load_data(filename="../data/mnist.pkl.gz"):
     """Return the MNIST data as a tuple containing the training data,
     the validation data, and the test data.
 
@@ -39,12 +40,12 @@ def load_data():
     That's done in the wrapper function ``load_data_wrapper()``, see
     below.
     """
-    f = gzip.open('../data/mnist.pkl.gz', 'rb')
+    f = gzip.open(filename, 'rb')
     training_data, validation_data, test_data = cPickle.load(f)
     f.close()
     return (training_data, validation_data, test_data)
 
-def load_data_wrapper():
+def load_data_wrapper(filename="../data/mnist.pkl.gz"):
     """Return a tuple containing ``(training_data, validation_data,
     test_data)``. Based on ``load_data``, but the format is more
     convenient for use in our implementation of neural networks.
@@ -65,7 +66,7 @@ def load_data_wrapper():
     the training data and the validation / test data.  These formats
     turn out to be the most convenient for use in our neural network
     code."""
-    tr_d, va_d, te_d = load_data()
+    tr_d, va_d, te_d = load_data(filename)
     training_inputs = [np.reshape(x, (784, 1)) for x in tr_d[0]]
     training_results = [vectorized_result(y) for y in tr_d[1]]
     training_data = zip(training_inputs, training_results)
